2edc35f252176fd6d73c3fba305b685a98995c87
Alexys Miranda <malexys482@gmail.com>
2026-07-16 12:05:38 -0600

    Add files via upload
    Se agregaron los botones a la pagina usando JavaScript, HTML y le generamos estilos desde Css

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btnSaludar").addEventListener("click", function() {
+    alert("Hola mundo¡¡¡");
+});
+
+document.getElementById("contacto").addEventListener("click", function() {
+    alert("Bienvenido a javascript¡¡¡");
+});
