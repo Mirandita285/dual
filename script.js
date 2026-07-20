3af7df33dd0dbc0fab9ea8e3ddcf799b597d52f7
Alexys Miranda <malexys482@gmail.com>
2026-07-20 11:37:03 -0600

    Add files via upload
    Pruebas condicionales

--- script.js
+++ script.js
@@ -61,3 +61,50 @@ let vejes = 20;
 let tieneLicencia = true;
 
 console.log(vejes >= 18 && "Es mayor de edad y tiene licencia ");
+
+
+
+// Ejercisios de condicion //
+
+if (vejes >= 18) {
+    console.log("Puedes ingresar a coco bergarden")
+}
+
+let saldo = 500;
+
+if (saldo >= 1000) {
+    console.log("Compra realizada");
+} else {
+    console.log("Saldo insuficiente")
+}
+
+let calificacion = 85;
+
+if (calificacion >= 90) {
+    console.log("Exelente tienes una beca por promedio");
+} else if (calificacion >=70) {
+    console.log("Muy bien, nunca cambies");
+} else {
+    console.log("Reprobado, suerte para la proxima.")
+}
+
+// Switch //
+let avenger = "Iron man";
+
+switch(avenger) {
+    case "Spiderman": console.log("No importa que todos me olviden con tal de salvar al mundo"); break;
+    case "Iron man": console.log("Bienvenido señor STARK"); break; 
+    case "Capitan America": console.log("Puedo hacer esto todo el dia"); break; 
+    case "Doctor Strange": console.log("La arrogancia y el miedo nos impiden aprender"); break; 
+    case "Vision": console,log("La humanidad es imperfecta y precisamente por eso es extraordinaria."); break;
+    default: console.log("otro dia");
+}
+
+// operador ternario //
+
+let compra = 1200;
+let descuento = (compra >= 1200) ? "10% de descuento" : "sin descuento";
+
+let temperatura = 37;
+let calor = (temperatura >= 20) ? "Hace calor" : "Hace frio";
+
